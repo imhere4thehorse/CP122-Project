e19ee7a4a939eab7ec452145ad833f0d4b2a9005
imhere4thehorse <106560789+imhere4thehorse@users.noreply.github.com>
2022-11-26 16:34:36 -0700

    Create Supermarket.java

---
+++ Supermarket.java
@@ -0,0 +1,27 @@
+import java.io.*;//for throws IOException
+import java.awt.Rectangle;
+
+public class Supermarket extends SuperObject2{
+  private static final int WIDTH = 40;
+  private static final int HEIGHT = 40;
+  boolean shop = true;
+
+  public Supermarket(GamePanel2 panel2, String fileName, Player2 player2) throws IOException{
+    super(panel2, fileName, player2);
+
+  }
+  public Rectangle getBounds() {
+		return new Rectangle(x, y, WIDTH, HEIGHT);
+	}
+  private boolean collision() {
+		return this.panel2.player2.getBounds().intersects(getBounds());
+	}
+  void move() {
+		if (collision() && shop){
+      this.panel2.SuperMarkets();
+      shop = false;
+		}
+	}
+}
+
+//refer to DoorObject for explanations on object subclass code
