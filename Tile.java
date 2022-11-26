1ab76812722bc59ad0a916923267d8a24fe5a9b0
imhere4thehorse <106560789+imhere4thehorse@users.noreply.github.com>
2022-11-26 16:35:41 -0700

    Create Tile.java

---
+++ Tile.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.Timer;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+public class Tile{
+//parent class of TileManager, Tiles2, TileManagerFight
+  public BufferedImage image;
+  public boolean collision = false;
+
+}
